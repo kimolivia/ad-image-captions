e401a5abac54a86d49344c83e7e6729a61d70247
fredcorpuz06 <fcorpuz@wesleyan.edu>
2018-10-29 17:31:41 -0400

    Define StreamListener

--- 01_scrape-wendys-twitter.py
+++ 01_scrape-wendys-twitter.py
@@ -11,6 +11,49 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+# print tweets on my home page
 public_tweets = api.home_timeline()
 for tweet in public_tweets:
     print(tweet.text)
+
+
+class MyStreamListener(tweepy.StreamListener):
+    def __init__(self, api=None):
+        super(MyStreamListener, self).__init__()
+        self.num_tweets = 0
+        self.file = open('tweets.txt', 'w')
+    
+    def on_status(self, status):
+        tweet = status._json
+        self.file.write(json.dumps(tweet) + '\n')
+        tweet_list.append(status)
+        self.num_tweets += 1
+        if self.num_tweets < 100:
+            return True
+        else:
+            return False
+        self.file.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
