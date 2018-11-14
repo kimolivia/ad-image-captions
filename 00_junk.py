7b4a47d4e4c3416156266b79bd9659b7f5a3237f
fredcorpuz06 <fcorpuz@wesleyan.edu>
2018-11-14 17:26:04 -0500

    Download all tweets, images + food training data

--- 00_junk.py
+++ 00_junk.py
@@ -1,6 +1,16 @@
-with open('twitter.pw', 'r') as f:
+""" with open('twitter.pw', 'r') as f:
     twitter = f.read()
 
 print(twitter, type(twitter), len(twitter))
 t = twitter.split('\n')
-print(t, len(t))
+print(t, len(t)) """
+
+
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show() 
