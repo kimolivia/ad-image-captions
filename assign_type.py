db72bb46ea2b315b7be8af3d73d5cbd3e061ed28
Olivia Kim <okim@wesleyan.edu>
2018-11-16 12:18:39 -0500

    Update assign_type.py

--- assign_type.py
+++ assign_type.py
@@ -43,18 +43,29 @@ class Type:
             rand_index = random.randint(0, 10)
             generated_types.append(self.food_types[rand_index])
         self.df["food type"] = generated_types
-        print(self.df.to_string())
+        #print(self.df.to_string())
 
 
 def main():
 
+    df_list = list()
     current_dir = os.getcwd()
-    file_path = os.path.join(str(current_dir), "output", "tweet_info", "Wendys-tweets-11-14-16-42.csv")
+    folder = os.path.join(str(current_dir), "output", "tweet_info")
+    all_files = os.listdir(folder)
+    for file in all_files:
+        if file.startswith("."):
+            pass
+        else:
+            file_path = os.path.join(folder, file)
+            new_df = Type(file_path).df
+            df_list.append(new_df)
+    big_df = pandas.concat(df_list)
+    print(big_df.to_string())
+    big_df.to_csv("dummy_food.csv", sep=',')
 
     if len(sys.argv) > 1:
         file_path = str(sys.argv[1])
 
-    classified = Type(file_path)
 
 if __name__ == "__main__":
     main()
